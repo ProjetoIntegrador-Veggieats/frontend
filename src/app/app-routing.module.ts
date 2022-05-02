4daba07e37558dcea776a2f3189041514b01e5d6
PedroHenriquedp <pereira.henrique@aluno.ufabc.edu.br>
2022-05-02 18:28:33 -0300

    Adicionando rota Entrar-Cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContatoComponent } from './contato/contato.component';
+import { EntrarCadastroComponent } from './entrar-cadastro/entrar-cadastro.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 import { SobreComponent } from './sobre/sobre.component';
@@ -10,9 +11,10 @@ const routes: Routes = [
   {path:"restaurantes", component:RestaurantesComponent},
   {path:"sobre", component:SobreComponent},
   {path:"inicio", component:InicioComponent},
-  {path:"contato", component:ContatoComponent}
-  
-  
+  {path:"contato", component:ContatoComponent},
+  {path:"login", component:EntrarCadastroComponent}
+
+
 ];
 
 @NgModule({
