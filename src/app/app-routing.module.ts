67fdcdc4d80b7c4db0de942f2722a5a09543047d
GeordanFS <geordansantos@gmail.com>
2022-05-02 15:16:05 -0300

    Cabeçalho e rodapé e algumas rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './contato/contato.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
+import { SobreComponent } from './sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"inicio",pathMatch:"full"},
+  {path:"restaurantes", component:RestaurantesComponent},
+  {path:"sobre", component:SobreComponent},
+  {path:"inicio", component:InicioComponent},
+  {path:"contato", component:ContatoComponent}
+  
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
