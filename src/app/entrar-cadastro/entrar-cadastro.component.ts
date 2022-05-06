0e4f2234887742f4b65a936ea8e1647c056346c8
GeordanFS <geordansantos@gmail.com>
2022-05-06 14:55:55 -0300

    Implementação de service e login e cadastro funcional

--- src/app/entrar-cadastro/entrar-cadastro.component.ts
+++ src/app/entrar-cadastro/entrar-cadastro.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Usuario } from '../model/Usuario';
+import { UsuarioLogin } from '../model/UsuarioLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-entrar-cadastro',
@@ -7,9 +12,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntrarCadastroComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario= new Usuario
+  confirmarSenha: string
+  tipoUsuario: string
+  usuarioLogin: UsuarioLogin= new UsuarioLogin
 
-  ngOnInit(): void {
+  constructor(private authService: AuthService,
+              private router: Router ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
+  }
+  
+  confirmSenha(event:any){
+    this.confirmarSenha=event.target.value
   }
 
+  tipoUser(event:any){
+    this.tipoUsuario=event.target.value
+  }
+
+  cadastrar(){
+      this.usuario.tipoUsuario=this.tipoUsuario
+
+      if(this.usuario.senha != this.confirmarSenha){
+      alert("As senhas estão incorretas")
+      }else{
+        this.authService.cadastrar(this.usuario).subscribe((resp: Usuario)=> {this.usuario=resp 
+        this.router.navigate(["/inicio"])
+        alert("Usuario cadastrado com sucesso!")}        
+        
+        )
+      }
+  }
+  entrar(){
+    this.authService.logar(this.usuarioLogin).subscribe({next:(resp: UsuarioLogin)=>{this.usuarioLogin=resp 
+      environment.token=this.usuarioLogin.token
+      environment.nome=this.usuarioLogin.nome
+      environment.foto=this.usuarioLogin.foto
+      environment.id=this.usuarioLogin.id
+
+      this.router.navigate(['/inicio'])}, 
+      error: erro=> {
+      if (erro.status==401) {
+        alert("Usuário ou senha incorretos")
+      }
+    }})
+}
 }
--- src/app/model/Usuario.ts
+++ src/app/model/Usuario.ts
@@ -1,6 +1,13 @@
+import { Produto } from "./Produto"
+
 export class Usuario{
     public id: number
     public nome: string
-    
+    public usuario: string
+    public foto: string
+    public senha:string
+    public cpf:string
+    public tipoUsuario:string
+    public Produto: Produto[]
     
 }
